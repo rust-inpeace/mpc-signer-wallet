230ef9586e7b98e496191c0efb383a804e255593
rust-inpeace <somyaranjankhatua122@gmail.com>
2025-04-05 23:12:44 +0530

    create transacton api for mpc wallet

--- src/routes/transaction.rs
+++ src/routes/transaction.rs
@@ -4,18 +4,10 @@ use std::sync::Arc;
 
 use crate::services::database::Database;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct TransactionRequest {
-    pub chain_id: String,
-    pub tx_type: String,
-    pub to: String,
-    pub from: String,
-    pub amount: u32
-}
-
+use super::handler::auth_handler::{TransactionRequest, UserServices};
 
 pub fn transaction_routes() -> Router {
     Router::new().route("/user/create/transaction", get(|Extension(db): Extension<Arc<Database>>, Json(payload): Json<TransactionRequest>| async move {
-
+        db.create_transaction(payload).await
     }))
 }
